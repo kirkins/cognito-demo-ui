932c475b9e44ccaa8c9dbf498ba5c0c8611c2d09
kirkins <kirkins@gmail.com>
2019-10-04 21:04:02 +0000

    make store persistent

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,33 @@ import thunk from 'redux-thunk'
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (!serializedState) return undefined;
+    else return JSON.parse(serializedState);
+  } catch(err) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+const persistedStore = loadState();
 const store = createStore(rootReducer,
+  persistedStore,
   storeEnhancers(applyMiddleware(thunk))
-)
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store
